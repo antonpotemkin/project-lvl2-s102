ed9118cf2c39b66695719038f6f80b0cc556c575
Anton Potemkin <79133891911@yandex.ru>
2017-07-03 16:45:30 +0700

    add commander, fix version

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
+const program = require('commander');
+
+program
+  .version('1.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'Output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .parse(process.argv);
+
 console.log('gendiff project');
