7a989d31820540425b84796d2d2ef2d563ed2a1f
Anton Potemkin <79133891911@yandex.ru>
2017-07-04 16:08:59 +0700

    refactor index, cli-buider

---
+++ src/cli-builder.js
@@ -0,0 +1,15 @@
+import program from 'commander';
+import packageJson from '../package.json';
+import comporator from './comporator';
+
+program
+  .version(packageJson.version)
+  .description(packageJson.description)
+  .option('-f, --format [type]', 'Output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig) => {
+    console.log(comporator(firstConfig, secondConfig));
+  })
+  .parse(process.argv);
+
+export default () => program.action;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
-import program from 'commander';
-import packageJson from '../package.json';
 import comporator from './comporator';
-
-const help = program
-  .version(packageJson.version)
-  .description(packageJson.description)
-  .option('-f, --format [type]', 'Output format')
-  .arguments('<firstConfig> <secondConfig>')
-  .action((cmd, env) => {
-    console.log(comporator(cmd, env));
-  })
-  .parse(process.argv);
-
-export const gendiff = () => help.action;
 
 export default comporator;
