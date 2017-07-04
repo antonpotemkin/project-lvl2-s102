f31bb4eb1186d50b6d759e89ed3d6f2ac1931fe0
Anton Potemkin <79133891911@yandex.ru>
2017-07-04 19:52:58 +0700

    fix cli-builder

--- src/cli-builder.js
+++ src/cli-builder.js
@@ -2,12 +2,13 @@ import program from 'commander';
 import packageJson from '../package.json';
 import comporator from './comporator';
 
-export default () => program
+program
   .version(packageJson.version)
   .description(packageJson.description)
   .option('-f, --format [type]', 'Output format')
   .arguments('<firstConfig> <secondConfig>')
   .action((firstConfig, secondConfig) => {
     console.log(comporator(firstConfig, secondConfig));
-  })
-  .parse(process.argv);
+  });
+
+export default () => program.parse(process.argv);
