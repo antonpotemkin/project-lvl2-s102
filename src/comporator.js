793da66ac461cd96213d15420007927f65dd3014
Anton Potemkin <79133891911@yandex.ru>
2017-07-05 10:05:28 +0700

    refactor comporator

--- src/comporator.js
+++ src/comporator.js
@@ -1,3 +1,4 @@
+import lodash from 'lodash';
 import reader from './readerJson';
 
 const div = '\n  ';
@@ -5,21 +6,17 @@ const div = '\n  ';
 export default (path1, path2) => {
   const beforeJson = reader(path1);
   const afterJson = reader(path2);
-  const changedJson = Object.keys(beforeJson)
+  const mixedKeys = lodash.union(Object.keys(beforeJson), Object.keys(afterJson));
+  const result = mixedKeys
     .reduce((acc, value) => {
       if (afterJson[value] === undefined) {
         return `${acc}${div}- ${value}: ${beforeJson[value]}`;
+      } else if (beforeJson[value] === undefined) {
+        return `${acc}${div}+ ${value}: ${afterJson[value]}`;
       } else if (afterJson[value] !== beforeJson[value]) {
         return `${acc}${div}+ ${value}: ${afterJson[value]}${div}- ${value}: ${beforeJson[value]}`;
       }
       return `${acc}${div}  ${value}: ${beforeJson[value]}`;
     }, '{');
-  const result = Object.keys(afterJson)
-    .reduce((acc, value) => {
-      if (beforeJson[value] === undefined) {
-        return `${acc}${div}+ ${value}: ${afterJson[value]}`;
-      }
-      return acc;
-    }, changedJson);
   return `${result}\n}`;
 };
