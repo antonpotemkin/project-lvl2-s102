3a912f70e8b4786d757d484b80dcc444faf16f3f
Anton Potemkin <79133891911@yandex.ru>
2017-07-05 20:34:42 +0700

    rename func

--- src/reader.js
+++ src/reader.js
@@ -2,16 +2,16 @@ import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
 
-const readerJson = query => JSON.parse(fs.readFileSync(`./${query}`, 'utf-8'));
+const readJson = query => JSON.parse(fs.readFileSync(`./${query}`, 'utf-8'));
 
-const readerYaml = query => yaml.safeLoad(fs.readFileSync(`./${query}`, 'utf-8'));
+const readYaml = query => yaml.safeLoad(fs.readFileSync(`./${query}`, 'utf-8'));
 
 export default (query) => {
   const { ext } = path.parse(query);
   switch (ext) {
     case '.yaml':
-      return readerYaml(query);
+      return readYaml(query);
     default:
-      return readerJson(query);
+      return readJson(query);
   }
 };
