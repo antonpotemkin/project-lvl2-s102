5ce34593bfadbedc2052004c74de158259ef52dd
Anton Potemkin <79133891911@yandex.ru>
2017-07-07 11:57:32 +0700

    refactor index, add ast-builder

--- src/index.js
+++ src/index.js
@@ -1,43 +1,6 @@
 import lodash from 'lodash';
 import read from './reader';
-
-// ast = [{ key:value, operation: operation, oldValue: oldValue, newValue: newValue}, ..., {}
-
-const buildAst = (firstConfig, secondConfig) => {
-  const mixedKeys = lodash.union(Object.keys(firstConfig), Object.keys(secondConfig));
-
-  const hasChildren = value => firstConfig[value] instanceof Object
-    || secondConfig[value] instanceof Object;
-
-  const result = mixedKeys
-    .map((value) => {
-      if (firstConfig[value] === undefined) {
-        const body = hasChildren(value) ? buildAst({}, secondConfig[value]) : secondConfig[value];
-        return { key: value,
-          operation: 'add',
-          newValue: body,
-        };
-      } else if (secondConfig[value] === undefined) {
-        const body = hasChildren(value) ? buildAst(firstConfig[value], {}) : firstConfig[value];
-        return { key: value,
-          operation: 'del',
-          oldValue: body,
-        };
-      } else if (firstConfig[value] !== secondConfig[value] && !hasChildren(value)) {
-        return { key: value,
-          operation: 'mod',
-          oldValue: firstConfig[value],
-          newValue: secondConfig[value],
-        };
-      }
-      const body = hasChildren(value) ?
-        buildAst(firstConfig[value], secondConfig[value]) : secondConfig[value];
-      return { key: value,
-        newValue: body,
-      };
-    });
-  return result;
-};
+import buildAst from './ast-builder';
 
 // level need to define quantity of indent
 // flag show parent operation
