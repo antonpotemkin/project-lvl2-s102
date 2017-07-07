7a9de9a39919c7cebf8d4a0a57c2333f8bb5493a
Anton Potemkin <79133891911@yandex.ru>
2017-07-07 11:36:12 +0700

    add ast, stringBuilder

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,21 +2,39 @@ import gendiff from '../src/';
 
 describe('Test gendiff', () => {
   const expected = `{
-    host: hexlet.io
-  + timeout: 20
-  - timeout: 50
-  - proxy: 123.234.53.22
-  + verbose: true
+    common: {
+        setting1: Value 1
+      - setting2: 200
+        setting3: true
+      - setting6: {
+            key: value
+        }
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+    }
+    group1: {
+      + baz: bars
+      - baz: bas
+        foo: bar
+    }
+  - group2: {
+        abc: 12345
+    }
+  + group3: {
+        fee: 100500
+    }
 }`;
 
-  it('#Json', () => {
+  it('#Json ext', () => {
     const pathBefore = '__tests__/fixtures/json/before.json';
     const pathAfter = '__tests__/fixtures/json/after.json';
     const actual = gendiff(pathBefore, pathAfter);
     expect(actual).toBe(expected);
   });
 
-  it('#Yaml', () => {
+  it('#Yaml ext', () => {
     const pathBefore = '__tests__/fixtures/yaml/before.yaml';
     const pathAfter = '__tests__/fixtures/yaml/after.yaml';
     const actual = gendiff(pathBefore, pathAfter);
--- src/index.js
+++ src/index.js
@@ -1,25 +1,78 @@
 import lodash from 'lodash';
 import read from './reader';
 
-const div = '\n  ';
+// ast = [{ key:value, operation: operation, oldValue: oldValue, newValue: newValue}, ..., {}
 
-const buildStringDiff = (firstConfig, secondConfig) => {
+const buildAst = (firstConfig, secondConfig) => {
   const mixedKeys = lodash.union(Object.keys(firstConfig), Object.keys(secondConfig));
+
+  const hasChildren = value => firstConfig[value] instanceof Object
+    || secondConfig[value] instanceof Object;
+
   const result = mixedKeys
-    .reduce((acc, value) => {
-      if (secondConfig[value] === undefined) {
-        return `${acc}${div}- ${value}: ${firstConfig[value]}`;
-      } else if (firstConfig[value] === undefined) {
-        return `${acc}${div}+ ${value}: ${secondConfig[value]}`;
-      } else if (secondConfig[value] !== firstConfig[value]) {
-        return `${acc}${div}+ ${value}: ${secondConfig[value]}${div}- ${value}: ${firstConfig[value]}`;
+    .map((value) => {
+      if (firstConfig[value] === undefined) {
+        const body = hasChildren(value) ? buildAst({}, secondConfig[value]) : secondConfig[value];
+        return { key: value,
+          operation: 'add',
+          newValue: body,
+        };
+      } else if (secondConfig[value] === undefined) {
+        const body = hasChildren(value) ? buildAst(firstConfig[value], {}) : firstConfig[value];
+        return { key: value,
+          operation: 'del',
+          oldValue: body,
+        };
+      } else if (firstConfig[value] !== secondConfig[value] && !hasChildren(value)) {
+        return { key: value,
+          operation: 'mod',
+          oldValue: firstConfig[value],
+          newValue: secondConfig[value],
+        };
       }
-      return `${acc}${div}  ${value}: ${firstConfig[value]}`;
+      const body = hasChildren(value) ?
+        buildAst(firstConfig[value], secondConfig[value]) : secondConfig[value];
+      return { key: value,
+        newValue: body,
+      };
+    });
+  return result;
+};
+
+// level need to define quantity of indent
+// flag show parent operation
+const buildString = (ast, level = 0, flag = false) => {
+  const div = '\n  ';
+  const indent = lodash.repeat('    ', level);
+  const result = ast
+    .reduce((acc, value) => {
+      const hasChildren = value.newValue instanceof Array || value.oldValue instanceof Array;
+      switch (value.operation) {
+        case 'add': {
+          const operation = flag ? '  ' : '+ ';
+          const body = hasChildren ? buildString(value.newValue, level + 1, true) : value.newValue;
+          return `${acc}${div}${indent}${operation}${value.key}: ${body}`;
+        }
+        case 'del': {
+          const operation = flag ? '  ' : '- ';
+          const body = hasChildren ? buildString(value.oldValue, level + 1, true) : value.oldValue;
+          return `${acc}${div}${indent}${operation}${value.key}: ${body}`;
+        }
+        case 'mod': {
+          return `${acc}${div}${indent}+ ${value.key}: ${value.newValue}`
+           + `${div}${indent}- ${value.key}: ${value.oldValue}`;
+        }
+        default: {
+          const body = hasChildren ? buildString(value.newValue, level + 1) : value.newValue;
+          return `${acc}${div}${indent}  ${value.key}: ${body}`;
+        }
+      }
     }, '{');
-  return `${result}\n}`;
+  return `${result}\n${indent}}`;
 };
 
 export default (firstQuery, secondQuery) => {
   const { firstConfig, secondConfig } = read(firstQuery, secondQuery);
-  return buildStringDiff(firstConfig, secondConfig);
+  const ast = buildAst(firstConfig, secondConfig);
+  return buildString(ast);
 };
